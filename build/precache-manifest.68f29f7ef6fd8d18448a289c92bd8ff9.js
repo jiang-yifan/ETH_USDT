4d13a7a857b97a5ba2ae078e35d66b4f760e0c6b
Yifan Jiang <yifan.jiang@thundertoken.com>
2019-08-06 17:41:11 +0800

    build

---
+++ build/precache-manifest.68f29f7ef6fd8d18448a289c92bd8ff9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba30a669c8aea1469b0cf207ff5e5e84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0d7feb4486248b19b5a",
+    "url": "/static/js/2.012d2f48.chunk.js"
+  },
+  {
+    "revision": "e9079a72aefc63e3a164",
+    "url": "/static/js/main.38785c7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
