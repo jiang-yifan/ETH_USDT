2964c5c4581b4e703a82bd00b3ec858d9dbf381f
Yifan Jiang <yifan.jiang@thundertoken.com>
2019-08-06 17:44:03 +0800

    update build location

---
+++ build/precache-manifest.5b945b19fc5a39bf32e17d362b6d858d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4999a88b295cd2ca6370e7e637c9443f",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "b0d7feb4486248b19b5a",
+    "url": "/build/static/js/2.012d2f48.chunk.js"
+  },
+  {
+    "revision": "e9079a72aefc63e3a164",
+    "url": "/build/static/js/main.38785c7a.chunk.js"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68f29f7ef6fd8d18448a289c92bd8ff9.js"
+  "/build/precache-manifest.5b945b19fc5a39bf32e17d362b6d858d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
